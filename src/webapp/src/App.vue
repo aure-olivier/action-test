e21244c980d22193441f17bcffaededeafe65147
Aurelien Olivier <aure.olivier@gmail.com>
2021-02-12 12:41:46 +0100

    CHANGE 1

--- src/webapp/src/App.vue
+++ src/webapp/src/App.vue
@@ -20,7 +20,7 @@ import Header from "./components/Headers";
 import VoiceMails from "./components/VoiceMails";
 import Interactions from "./components/interactions/Interactions";
 import ServiceNotifications from "./components/ServiceNotifications";
-import {mapGetters} from 'vuex'
+import {mapGetter} from 'vuex'
 
 export default {
   name: "App",
